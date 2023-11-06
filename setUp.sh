b22fb218c9236a3fa8c1024bb5e6ac06a231c836
Jor <jordansar2001@gmail.com>
2023-11-06 19:37:46 +0000

    packages, preload, mirrors and comment

--- setUp.sh
+++ setUp.sh
@@ -1,3 +1,33 @@
 #! /bin/bash
+#
+#Udate
 sudo pacman -Syyu
+
+# YAY
 cd ~/Downloads/ && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ~
+
+# Configure PacMan and mirrors 
+sudo nano /etc/pacman.conf
+sudo pacman -Sy
+sudo pacman -S reflector
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+
+#Bluetooth
+
+sudo pacman -S bluez blueman bluez-utils
+sudo modprobe btusb
+sudo systemctl enable bluetooth && sudo systemctl start bluetooth
+
+# Install basic utilities
+
+sudo pacman -S p7zip unrar tar rsync git neofetch htop exfat-utils fuse-exfat ntfs-3g flac jasper aria2 jdk-openjdk flatpak libreoffice-fresh vlc gimp 
+
+# Preload
+yay -S preload
+sudo systemctl enable preload && sudo systemctl start preload
+
+# timeshift
+yay -Sy timeshift
+
